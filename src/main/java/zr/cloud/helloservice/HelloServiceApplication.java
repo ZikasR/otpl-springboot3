03e2422fe9505e9e32f9453019dba0ff22b69069
“Zakaria <zakaria.ra@gmail.com>
2024-05-14 16:45:17 +0200

    Add Zipkin & Jaeger for tracing

--- src/main/java/zr/cloud/helloservice/HelloServiceApplication.java
+++ src/main/java/zr/cloud/helloservice/HelloServiceApplication.java
@@ -1,14 +1,47 @@
 package zr.cloud.helloservice;
 
-import java.util.Objects;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
 public class HelloServiceApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(HelloServiceApplication.class, args);
+	}
+
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@RestController
+	class HelloController {
+
+		private final RestTemplate restTemplate;
+
+		HelloController(RestTemplate restTemplate) {
+			this.restTemplate = restTemplate;
+		}
+
+		@GetMapping("/hello")
+		public String hello() {
+			ResponseEntity<String> responseEntity = this.restTemplate.postForEntity("https://httpbin.org/post", "Hello, Cloud", String.class);
+			return responseEntity.getBody();
+		}
+		
+		public void test() {
+			System.out.println("Hello World");
+		}
 	}
 
 }
